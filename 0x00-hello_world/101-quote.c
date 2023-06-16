bdc534fdfd1fb34bc88e3450e11ce32f8873365d
Ndoro <ndoromoses39@gmail.com>
2023-06-16 07:08:02 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+/**
+ * main - main block
+ * return: 1
+ */
+int main(void)
+{
+	char *sentence = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	write(2, sentence, 49);
+	write(2, "\n", 1);
+	return (1);
+}
