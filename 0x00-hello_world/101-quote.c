f4f2af7503287df7747ec18d74d24e1b95795341
Ndoro <ndoromoses39@gmail.com>
2023-06-16 07:48:09 -0700

    Quote modified for right output ++

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	char *sentence = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-	write(2,sentence, 49);
+	write(2,sentence, 58);
 	write(2, "\n", 1);
 	return (1);
 }
