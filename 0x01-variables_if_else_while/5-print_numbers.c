49c085007940e9b4bd064eed47bbda65a0ec1cc1
Ndoro <ndoromoses39@gmail.com>
2023-06-17 03:03:02 -0700

    Numbers to base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+
+	return (0);
+}
