e06f2813fa48c177cfe78512c8a6af8176a660b8
Ndoro <ndoromoses39@gmail.com>
2023-06-17 02:27:30 -0700

    The last digit modification

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 	}
 	else if (value == 0)
 	{
-		printf("Last digit of %d is %d and is zero\n", n, value);
+		printf("Last digit of %d is %d and is 0\n", n, value);
 	}
 	else if (value < 6 && value != 0)
 	{
