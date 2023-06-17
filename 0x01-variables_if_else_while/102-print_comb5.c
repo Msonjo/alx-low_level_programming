538630bbe34281a2c0bf7b13feb6f0fcdc6667f9
Ndoro <ndoromoses39@gmail.com>
2023-06-17 04:30:28 -0700

    All possible combinations of two two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int valueOne = 0, valueTwo;
+
+	while (valueOne <= 99)
+	{
+		valueTwo = valueOne;
+		while (valueTwo <= 99)
+		{
+			if (valueTwo != valueOne)
+			{
+				putchar((valueOne / 10) + 48);
+				putchar((valueOne % 10) + 48);
+				putchar(' ');
+				putchar((valueTwo / 10) + 48);
+				putchar((valueTwo % 10) + 48);
+			}
+			else if (valueOne != 98 || valueTwo != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			++valueTwo
+		}
+		++valueOne;
+	}
+	putchar('\n');
+	return (0);
+}
