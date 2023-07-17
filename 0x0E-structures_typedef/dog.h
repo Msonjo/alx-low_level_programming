184cfc4f2570193551b0b09bc868abc7df78c7f6
Msonjo <ndoromoses39@gmail.com>
2023-07-17 05:39:45 -0700

    Dog and Dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,14 +14,14 @@ struct dog
 	char *owner;
 };
 /**
- * dogT - typedef for struct dog
+ * dog_t - typedef for struct dog
  */
-typedef struct dog dogT;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dogT *new_dog(char *name, float age, char *owner);
-void free_dog(dogT *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
